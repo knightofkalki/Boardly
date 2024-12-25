bbc135487f9f3360b88873c42cf1615a99f9b4d6
arvind <achurox1121@gmail.com>
2024-12-25 18:57:58 +0530

    fix scrolling overflow

--- src/pages/ChapterList.jsx
+++ src/pages/ChapterList.jsx
@@ -36,7 +36,7 @@ export default function ChapterList() {
   )
 
   return (
-    <div className="min-h-screen bg-[#F6F8FC]">
+    <div className=" bg-[#F6F8FC]">
       <div className="max-w-5xl mx-auto p-4 sm:p-6">
         <div className="flex items-center mb-6">
           <button 
