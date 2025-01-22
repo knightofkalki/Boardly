17093b8f67e79d429433de414ffb4f34d7e19522
Upayan Mazumder <upayanm3@gmail.com>
2025-01-22 23:09:00 +0530

    Add Demo page and route to application

---
+++ src/pages/Demo.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+export default function Demo() {
+    return (
+        <div className="container mx-auto min-w-[70vw] p-6 rounded-lg bg-white">
+            <div className="mb-4">
+                <h1 className="text-3xl text-orange-600 font-bold">PYQs</h1>
+            </div>
+
+        </div>
+    );
+};
